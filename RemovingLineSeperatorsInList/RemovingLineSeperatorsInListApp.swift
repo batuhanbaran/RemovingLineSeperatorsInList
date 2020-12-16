9f8c5b38a34c079d48da857bae44243ebdd2e48b
batuhanbaran <fb_batuhan_baran@hotmail.com>
2020-12-16 20:11:35 +0300

    Initial Commit

---
+++ RemovingLineSeperatorsInList/RemovingLineSeperatorsInListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemovingLineSeperatorsInListApp.swift
+//  RemovingLineSeperatorsInList
+//
+//  Created by Batuhan Baran on 16.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct RemovingLineSeperatorsInListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
