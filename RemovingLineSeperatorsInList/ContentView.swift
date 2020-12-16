36460aeb7e7d241aec286382b0f93ecbc03efd1a
batuhanbaran <fb_batuhan_baran@hotmail.com>
2020-12-16 20:14:28 +0300

    First Commit

--- RemovingLineSeperatorsInList/ContentView.swift
+++ RemovingLineSeperatorsInList/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let urunler = [
+        
+        Urunler(isim: "Süt", resim: "food"),
+        Urunler(isim: "Peynir", resim: "food"),
+        Urunler(isim: "Ekmek", resim: "food"),
+        Urunler(isim: "Kola", resim: "food"),
+        Urunler(isim: "Gazoz", resim: "food"),
+        Urunler(isim: "Bisküvi", resim: "food")
+    ]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            ScrollView{
+                
+                LazyVStack(spacing: 25){
+                    
+                    ForEach(urunler, id: \.id){ urun in
+                        
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame(width: UIScreen.main.bounds.width - 25, height: 60, alignment: .center)
+                            .foregroundColor(Color(.systemGray6))
+                            
+                            .overlay(
+                                
+                                HStack{
+                                    
+                                    Image(urun.resim)
+                                        .resizable()
+                                        .frame(width: 30, height: 30, alignment: .center)
+                                        .padding(.leading, 20)
+                                    
+                                    Text(urun.isim)
+                                        .padding(.leading, 5)
+                                    
+                                    Spacer()
+                                }
+                            )
+                        
+                    }
+                    
+                }.edgesIgnoringSafeArea(.all)
+                .navigationBarTitle("Ürünler")
+            }
+            
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        
         ContentView()
+        
     }
 }
---
+++ RemovingLineSeperatorsInList/Urunler.swift
@@ -0,0 +1,17 @@
+//
+//  Urunler.swift
+//  PraticeWithSwiftUI
+//
+//  Created by Batuhan Baran on 16.12.2020.
+//
+
+import Foundation
+
+
+struct Urunler: Identifiable {
+    
+    var id = UUID()
+    var isim: String
+    var resim: String
+    
+}
